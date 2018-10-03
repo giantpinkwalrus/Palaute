82c41d3367711a85dbfca1b0920fca07cbac824f
Sampo Lamminsalo <sampo.lamminsalo@meiko.fi>
2018-10-04 01:26:02 +0300

    Very rough draft of pagination

--- lib/palaute_web/controllers/dashboard_controller.ex
+++ lib/palaute_web/controllers/dashboard_controller.ex
@@ -1,7 +1,37 @@
 defmodule PalauteWeb.DashboardController do
+  import Ecto.Query
   use PalauteWeb, :controller
+  alias Palaute.{Repo, Feedback}
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  @per_page 1 
+  #these should probably go somewhere else
+  defp order(query, params) do
+    cond do
+      Dict.has_key?(params, "first") ->
+        from q in query, order_by: [desc: :id]
+      true ->
+        from q in query, order_by: q.id
+    end
+  end
+
+  defp paginate(query, params) do
+    cond do
+      Dict.has_key?(params, "first") ->
+        from q in query, where: q.id < ^params["first"]
+      Dict.has_key?(params, "last") ->
+        from q in query, where: q.id > ^params["last"]
+      true ->
+        from q in query
+    end
+  end
+
+  def index(conn, params) do
+    feedback = Feedback
+            |> limit(@per_page)
+            |> order(params)
+            |> paginate(params)
+            |> Repo.all()
+	
+    render conn, "index.html", feedback: feedback
   end
 end
